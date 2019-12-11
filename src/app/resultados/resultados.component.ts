57911c0af64eb84325750e6ddede7a26e2ce1138
jesuel Merudia Calderon <jesue2000@gmail.com>
2019-12-11 11:31:23 -0400

    añadiendo datos base para las graficas

--- src/app/resultados/resultados.component.ts
+++ src/app/resultados/resultados.component.ts
@@ -21,14 +21,15 @@ export class ResultadosComponent implements OnInit {
       }
     }
   };
-  public barChartLabels: Label[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  public barChartLabels: Label[] = ['tiempo promedio','parametro'];
   public barChartType: ChartType = 'bar';
   public barChartLegend = true;
   public barChartPlugins = [pluginDataLabels];
 
   public barChartData: ChartDataSets[] = [
-    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
-    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' }
+    { data: [80, 59], label: 'Destacada' },
+    { data: [228, 98], label: 'Pesima' },
+    { data: [128, 78], label: 'Actual' }
   ];
 
   constructor() { }
